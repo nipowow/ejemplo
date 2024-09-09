9f4d1fa77313fef0b955360c1c7aad5f31fccb57
nipowow <nicolas.poggio@davinci.edu.ar>
2024-09-09 10:11:04 -0300

    agrego xd

--- src/holqaa/main.java
+++ src/holqaa/main.java
@@ -1,7 +1,10 @@
 package holqaa;
 
+import javax.swing.JOptionPane;
+
 public class main {
 	public main() {
+		JOptionPane.showMessageDialog(null, "hola crack");
 		// TODO Auto-generated constructor stub
 	}
 }
